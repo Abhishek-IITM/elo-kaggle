ad6ee362f8a7c341fa10263e7cf4449cbd8c474f
Abhishek <abhishek.agrawal36@gmail.com>
2018-12-09 17:15:25 +0530

    Minor changes

--- features/month_lag_feature_maker.py
+++ features/month_lag_feature_maker.py
@@ -17,10 +17,13 @@ month_lag_features.rename(columns={"amin":"min_month_lag", "amax":"max_month_lag
                                    "nunique":"unique_transacted_months"},inplace=True)
 
 
-
 monthly_freq = (id_month.groupby(["card_id", "month_lag"]).agg({"dummy":np.sum}).reset_index().groupby("card_id")
                 .agg({"dummy":[np.max, np.mean]}).reset_index())
 monthly_freq = pd.DataFrame([monthly_freq["card_id"], monthly_freq["dummy"]["amax"], monthly_freq["dummy"]["mean"]]).T
 monthly_freq.rename(columns={"amax":"max_monthly_freq", "mean":"mean_monthly_freq"}, inplace=True)
 
+
+month_lag_features = month_lag_features.merge(monthly_freq, how="inner", on="card_id")
+
 month_lag_features.to_csv("{}/{}.csv".format(path.feature_path,"month_lag_features"))
+del monthly_freq
