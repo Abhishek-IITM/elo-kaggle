278e9cc7960483e1b61444f79277727f2786ccbb
Abhishek <abhishek.agrawal36@gmail.com>
2019-01-30 11:18:42 +0530

    Amended metadata feature maker to generate unique record per card_id

--- services/distinct_levels_and_mode.py
+++ services/distinct_levels_and_mode.py
@@ -24,6 +24,10 @@ def get_unique_count_and_mode(source_df, field, path=path.path, nrows=None):
         _log.exception(e)
     _log.info("Successfully read from {}".format(source_df))
 
+    if sum(pd.isnull(df[field])):
+        field_mode = df[field].mode()[0]
+        df.loc[pd.isnull(df[field]), field] = field_mode
+
     _log.info("Fetching no. of distinct merchants transacted on")
     df_uniq = df.groupby("card_id").agg({field: pd.Series.nunique}).reset_index().rename(columns={field: "{}_{}_{}".format(prefix, "unique", field)})
 
@@ -31,6 +35,8 @@ def get_unique_count_and_mode(source_df, field, path=path.path, nrows=None):
     df_agg = df.groupby(["card_id", field]).agg({field: np.count_nonzero}).rename(columns={field: "count"}).reset_index()
     df_max = df_agg.groupby("card_id").agg({"count": np.max}).reset_index()
     df_max = df_agg.merge(df_max, how="inner", on=["card_id", "count"]).drop_duplicates().reset_index(drop=True)
+    df_max['rank'] = df_max.groupby(['card_id']).cumcount() + 1
+    df_max = df_max[df_max['rank'] == 1]
     df = df_max.merge(df_uniq, on="card_id", how="inner")
     df.rename(columns={"count":"{}_{}_{}".format(prefix, "max_count", field)}, inplace=True)
 
